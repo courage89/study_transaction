5cc160bc7fe326fe0568c909d03982cd7d3ce687
xuya <xuya@changingedu.com>
2016-10-21 09:10:38 +0800

    Unittest

---
+++ src/test/java/com/qingqing/study/TestBase.java
@@ -0,0 +1,21 @@
+package com.qingqing.study;
+
+import com.qingqing.common.util.JsonUtil;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+        "classpath:applicationContext.xml",
+})
+public class TestBase {
+
+    protected void printJson(Object object){
+        System.out.println(JsonUtil.getJsonFromObject(object));
+    }
+
+}
--- src/test/java/com/qingqing/study/dao/impl/SimpleUserDaoImplTest.java
+++ src/test/java/com/qingqing/study/dao/impl/SimpleUserDaoImplTest.java
@@ -1,31 +1,29 @@
 package com.qingqing.study.dao.impl;
 
 import com.qingqing.common.util.JsonUtil;
+import com.qingqing.study.TestBase;
 import com.qingqing.study.dao.TSimpleUserEntityDao;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
 
 /**
  *
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {
-        "classpath:applicationContext.xml",
-})
-public class SimpleUserDaoImplTest{
 
-    @Resource(name = "simpleUserEntitySFDao")
-//    @Resource(name = "simpleUserEntityHTDao")
+public class SimpleUserDaoImplTest extends TestBase {
+
+//    @Resource(name = "simpleUserEntitySFDao")
+    @Resource(name = "simpleUserEntityHTDao")
     private TSimpleUserEntityDao simpleUserDao;
 
     @Test
     public void testDao(){
-        System.out.println(JsonUtil.getJsonFromObject(simpleUserDao.findAll()));
+//        System.out.println(JsonUtil.getJsonFromObject(simpleUserDao.findAll()));
+
+        int id = 1;
+        System.out.println(JsonUtil.getJsonFromObject(simpleUserDao.findById(id)));
     }
+
+
 }
